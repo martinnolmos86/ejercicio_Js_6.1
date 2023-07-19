1aa8e3e30183ca4d0916044e4f78a97a0ddff77a
Martin Olmos <martinnolmos@gmail.com>
2023-07-19 08:47:54 -0300

    first commir

---
+++ ejercicio_6.1.js
@@ -0,0 +1,7 @@
+const nombresFamilia = new Set(["Fernanda", "Maria Marta", "Omar", "Marta"]);
+
+nombresFamilia.add("Juan");
+
+nombresFamilia.add("Javascript");
+
+console.log("Nombres de la familia:", nombresFamilia);
